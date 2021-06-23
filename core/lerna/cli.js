fefc61155c0e0710206ce057e1533375f46c55e3
zhaoziwei <1728434772@qq.com>
2021-06-24 00:42:24 +0800

    feat: 添加注释

--- core/lerna/cli.js
+++ core/lerna/cli.js
@@ -8,5 +8,6 @@ const importLocal = require("import-local");
 if (importLocal(__filename)) {
   require("npmlog").info("cli", "using local version of lerna");
 } else {
+  // reqyure(".") 是按照相对路径， 一个点为引入当前目录，没有写具体文件名，则默认引入./index.js
   require(".")(process.argv.slice(2));
 }
